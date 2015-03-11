7b3706eaa0093d8f5ba65af8ed590b6fcb3fe7cf
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-11 00:28:25 -0400

    lint

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
  * arr-flatten <https://github.com/jonschlinkert/arr-flatten>
  *
  * Copyright (c) 2014-2015, Jon Schlinkert.
- * Licensed under the MIT License
+ * Licensed under the MIT License.
  */
 
 'use strict';
