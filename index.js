4950a67f2e7eba5039ae6d2c365303bc606f2746
sricharankrishnan <sricharan.krishnan@gmail.com>
2021-12-30 12:14:20 +0530

    [Feature Update]: Recommendation
    - Recommendation to handle cases for deep nested array
    - Ref open issue from https://github.com/jonschlinkert/arr-flatten/issues/5

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-/*!
- * arr-flatten <https://github.com/jonschlinkert/arr-flatten>
- *
- * Copyright (c) 2014-2017, Jon Schlinkert.
- * Released under the MIT License.
- */
+/**
+  * Hello Jon.
+  *
+  * I saw the issue with the deep nested array and I came up with this.
+  * Let me know what you think. The only caveat I can see is that if there
+  * are any characters with square brackets, they will be affected.
+  *
+  * Cheers!
+  **/
 
 'use strict';
 
-module.exports = function (arr) {
-  return flat(arr, []);
-};
-
-function flat(arr, res) {
-  var i = 0, cur;
-  var len = arr.length;
-  for (; i < len; i++) {
-    cur = arr[i];
-    Array.isArray(cur) ? flat(cur, res) : res.push(cur);
+module.exports = (arr) => {
+  /* if not an array, then do not proceed */
+  if (!Array.isArray(arr)) {
+    throw new Error("- requires 'arr' argument to be of array type.");
   }
-  return res;
-}
+  /* ok. next */
+  else {
+    /* stringify the entire entry */
+    let stringifiedArray = JSON.stringify(arr);
+
+    /* remove all square brackets */
+    let stringWithoutSquareBrackets = stringifiedArray.split("[").join("").split("]").join("");
+
+    /* now return the value like this */
+    return JSON.parse(`[${stringWithoutSquareBrackets}]`);
+  }
+};
